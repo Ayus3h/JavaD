2030615dbd907e4ad5b00119cac7e47edc1a698d
Ayus3h <ayushisgentle@gmail.com>
2023-09-16 10:24:25 +0530

    First Commit

---
+++ Launch2.java
@@ -0,0 +1,9 @@
+
+public class Launch2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Started Pattern Today 20-06-2023");
+		System.out.println("Started ");
+	}
+
+}
